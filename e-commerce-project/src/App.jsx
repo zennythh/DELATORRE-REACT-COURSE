4a083e687e425969c0d95161dfdb1add477189fc
zennythh <delatorre.giancarlobscs2024@gmail.com>
2026-08-09 12:10:36 +0800

    Added orders page

--- e-commerce-project/src/App.jsx
+++ e-commerce-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
     <Route index element={<HomePage/>} /> 
     <Route path="checkout" element={<CheckoutPage/>} />
+    <Route path="orders" element={<OrdersPage/>} />
     </Routes>
   )
 }
