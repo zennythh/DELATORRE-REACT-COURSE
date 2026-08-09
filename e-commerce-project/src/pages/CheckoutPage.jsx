9f484e24e61038d2b5485f9e4468aeef99ba8e09
zennythh <delatorre.giancarlobscs2024@gmail.com>
2026-08-09 11:58:44 +0800

    Updated links to match the router

--- e-commerce-project/src/pages/CheckoutPage.jsx
+++ e-commerce-project/src/pages/CheckoutPage.jsx
@@ -9,7 +9,7 @@ export function CheckoutPage(){
       <div className="checkout-header">
         <div className="header-content">
           <div className="checkout-header-left-section">
-            <a href="index.html">
+            <a href="/">
               <img className="logo" src="images/logo.png" />
               <img className="mobile-logo" src="images/mobile-logo.png" />
             </a>
@@ -17,7 +17,7 @@ export function CheckoutPage(){
   
           <div className="checkout-header-middle-section">
             Checkout (<a className="return-to-home-link"
-              href="index.html">3 items</a>)
+              href="/">3 items</a>)
           </div>
   
           <div className="checkout-header-right-section">
