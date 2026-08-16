6df60886694de583ee475bdcbc9f68e1a95f3e3f
zennythh <delatorre.giancarlobscs2024@gmail.com>
2026-08-16 13:39:28 +0800

    Converted money utils and header component to TS

--- ecommerce-project/src/utils/money.ts
+++ ecommerce-project/src/utils/money.ts
@@ -1,4 +1,4 @@
-export function formatMoney(amountCents){
+export function formatMoney(amountCents: number){
   if (amountCents >= 0) {
   return `$${(amountCents / 100).toFixed(2)}`
   } else if (amountCents < 0){
