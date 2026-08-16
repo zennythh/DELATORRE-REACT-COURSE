fca4e67870dbfcd949bffde03ee36c200c08b025
zennythh <delatorre.giancarlobscs2024@gmail.com>
2026-08-16 09:33:20 +0800

    Fixed vite config

--- e-commerce-project/vite.config.js
+++ e-commerce-project/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './setupTests.js',
+  server: {
+    proxy: {
+      '/api':{
+        target: 'http://localhost:3000'
+      },
+      '/images':{
+        target: 'http://localhost:3000'
+      }
+    }
   }
-});
+  });
